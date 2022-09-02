91966fbe74b0f7d25ffc49c2d30fd14766e6b31e
Alex Chaphiv <achaphiv@gmail.com>
2022-09-01 23:11:58 -0400

    feat: add images

---
+++ src/images/index.ts
@@ -0,0 +1,155 @@
+function toPath(name: string) {
+  return `/images/${name}.png`
+}
+
+export const spaghettiWave = toPath(
+  '1662051636_scale_13.8_steps_135_seed_7224028837179423066_upscaled_2_sharpened_2'
+)
+
+export const runningAway = toPath(
+  '1662051519_scale_13.8_steps_135_seed_7224028837179423064_upscaled_2_sharpened_2'
+)
+
+export const ball = toPath(
+  '1662051693_scale_13.8_steps_135_seed_7224028837179423067_upscaled_2_sharpened_2'
+)
+
+export const knife = toPath(
+  '1662044319_scale_13.8_steps_160_seed_6064603097116983951_upscaled_2_sharpened_2'
+)
+
+export const swirlingActionShot = toPath(
+  '1662051578_scale_13.8_steps_135_seed_7224028837179423065_upscaled_2_sharpened_2'
+)
+
+export const strandsHangingOff = toPath(
+  '1662051403_scale_13.8_steps_135_seed_6038819540852106412_upscaled_2_sharpened_2'
+)
+
+export const atTheLowestPoint = toPath(
+  '1662051346_scale_13.8_steps_135_seed_6038819540852106411_upscaled_2_sharpened_2'
+)
+
+export const staringAtFist = toPath(
+  '1662051460_scale_13.8_steps_135_seed_7224028837179423063_upscaled_2_sharpened_2'
+)
+
+export const hereIsAPlate = toPath(
+  '1662044857_scale_13.8_steps_160_seed_1409599838619442385_upscaled_2_sharpened_2'
+)
+
+export const surpriseCloseUp = toPath(
+  '1662051221_scale_13.8_steps_135_seed_6038819540852106409_upscaled_2_sharpened_2'
+)
+
+export const strandsBehind = toPath(
+  '1662050970_scale_13.8_steps_135_seed_6038819540852106405_upscaled_2_sharpened_2'
+)
+
+export const theStance = toPath(
+  '1662050907_scale_13.8_steps_135_seed_6038819540852106404_upscaled_2_sharpened_2'
+)
+
+export const strandsLeftHand = toPath(
+  '1662051158_scale_13.8_steps_135_seed_6038819540852106408_upscaled_2_sharpened_2'
+)
+
+export const blackStuffBehind = toPath(
+  '1662051033_scale_13.8_steps_135_seed_6038819540852106406_upscaled_2_sharpened_2'
+)
+
+export const heBelieves = toPath(
+  '1662049612_scale_13.8_steps_135_seed_3568936245512176807_upscaled_2_sharpened_2'
+)
+
+export const NANI = toPath(
+  '1661990322_scale_13.8_steps_160_seed_6392953606761677951_upscaled_2_sharpened_2'
+)
+
+export const somethingOnLeftHand = toPath(
+  '1662051095_scale_13.8_steps_135_seed_6038819540852106407_upscaled_2_sharpened_2'
+)
+
+export const miniBlackStrands = toPath(
+  '1662050782_scale_13.8_steps_135_seed_6038819540852106402_upscaled_2_sharpened_2'
+)
+
+export const attack = toPath(
+  '1662049487_scale_13.8_steps_135_seed_3568936245512176805_upscaled_2_sharpened_2'
+)
+
+export const glowingForehead = toPath(
+  '1662045376_scale_13.8_steps_160_seed_3124957833307070032_upscaled_2_sharpened_2'
+)
+
+export const mirrorBreaking = toPath(
+  '1662050657_scale_13.8_steps_135_seed_3992971910009714279_upscaled_2_sharpened_2'
+)
+
+export const glowingStrands = toPath(
+  '1662048517_scale_13.8_steps_135_seed_3681495938669754122_upscaled_2_sharpened_2'
+)
+
+export const glowingStrandsTwo = toPath(
+  '1662048026_scale_13.8_steps_135_seed_3681495938669754114_upscaled_2_sharpened_2'
+)
+
+export const swingingAround = toPath(
+  '1662049074_scale_13.8_steps_135_seed_3568936245512176798_upscaled_2_sharpened_2'
+)
+
+export const hanging = toPath(
+  '1662049302_scale_13.8_steps_135_seed_3568936245512176802_upscaled_2_sharpened_2'
+)
+
+export const landing = toPath(
+  '1662044593_scale_13.8_steps_160_seed_6064603097116983955_upscaled_2_sharpened_2'
+)
+
+export const theBestMeal = toPath(
+  '1662045726_scale_13.8_steps_160_seed_3124957833307070037_upscaled_2_sharpened_2'
+)
+
+export const notYours = toPath(
+  '1662045443_scale_13.8_steps_160_seed_3124957833307070033_upscaled_2_sharpened_2'
+)
+
+export const youStayHere = toPath(
+  '1662046092_scale_13.8_steps_160_seed_3124957833307070042_upscaled_2_sharpened_2'
+)
+
+export const monster = toPath(
+  '1661910461_scale_7_steps_160_seed_8676019417614107648_upscaled_2_sharpened_2'
+)
+
+export const flimpsy = toPath(
+  '1662051868_scale_13.8_steps_135_seed_7224028837179423070'
+)
+
+export const notGood = toPath(
+  '1662046459_scale_13.8_steps_160_seed_3124957833307070047_upscaled_2_sharpened_2'
+)
+
+export const sadge = toPath(
+  '1662046898_scale_13.8_steps_160_seed_3124957833307070053_upscaled_2_sharpened_2'
+)
+
+export const aboutToScream = toPath(
+  '1662086517_scale_13.8_steps_135_seed_1185883968230660570_upscaled_2_sharpened_2'
+)
+
+export const scream = toPath(
+  '1662086210_scale_13.8_steps_135_seed_1185883968230660565_upscaled_2_sharpened_2'
+)
+
+export const blueMonster1 = toPath(
+  '1662086579_scale_13.8_steps_135_seed_1185883968230660571_upscaled_2_sharpened_2'
+)
+
+export const blueMonster2 = toPath(
+  '1662086641_scale_13.8_steps_135_seed_1185883968230660572_upscaled_2_sharpened_2'
+)
+
+export const arrival = toPath(
+  '1662051809_scale_13.8_steps_135_seed_7224028837179423069_upscaled_2_sharpened_2'
+)
